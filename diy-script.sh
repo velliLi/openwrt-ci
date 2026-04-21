d6ea4dbb1811fe98cd98d18be629d1bef2ffca59
velliLi <164885533+velliLi@users.noreply.github.com>
2026-04-21 23:00:58 +0800

    Enable mosdns and its Luci app
    Add configuration options for mosdns package.

--- diy-script.sh
+++ diy-script.sh
@@ -59,6 +59,10 @@ CONFIG_PACKAGE_luci-app-passwall2_INCLUDE_SingBox=y
 CONFIG_PACKAGE_lucky=y
 CONFIG_PACKAGE_luci-app-lucky=y
 
+#mosdns
+CONFIG_PACKAGE_mosdns=y
+CONFIG_PACKAGE_luci-app-mosdns=y
+
 # 默认主题设置
 CONFIG_PACKAGE_luci-theme-argon=y
 CONFIG_PACKAGE_luci-app-argon-config=y
